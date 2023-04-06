96b963e7961baa15ede3509990156ec07882bcdd
Lizard-LZD <wangziqi0515@gmail.com>
2023-04-06 04:14:30 +0100

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import './App.css'
 
-function App() {
+export default function App() {
+  // Create a useForm instance
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+  // Define a function to handle form submission
+  const onSubmit = (data) => {
+
+    fetch("https://0534904d-85da-4995-920b-6058237d7542.mock.pstmn.io/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        alert("Your personal information has been submitted successfully!");
+      })
+      .catch((error) => {
+        alert(error);
+      })
+      .finally(reset());
+  };
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Submit Information Form</h1>
+      {/* Use the handleSubmit function to wrap the onSubmit function */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* Use the register function to register each input field */}
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          type="text"
+          {...register("name", { required: true })}
+        />
+        {/* Use the errors object to display error messages */}
+        {errors.name && <p>Name is required</p>}
+
+        <label htmlFor="birthday">Birthday(YYYY-MM-DD)</label>
+        <input
+          id="birthday"
+          type="text"
+          {...register("birthday", {
+            required: true,
+            pattern: /^\d{4}-\d{2}-\d{2}$/,
+          })}
+        />
+        {errors.birthday && errors.birthday.type === "required" && (
+          <p>Birthday is required</p>
+        )}
+        {errors.birthday && errors.birthday.type === "pattern" && (
+          <p>Birthday is invalid</p>
+        )}
+
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          {...register("email", {
+            required: true,
+            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+          })}
+        />
+        {errors.email && errors.email.type === "required" && (
+          <p>Email is required</p>
+        )}
+        {errors.email && errors.email.type === "pattern" && (
+          <p>Email is invalid</p>
+        )}
+
+        <label htmlFor="phone">Phone</label>
+        <input
+          id="phone"
+          type="tel"
+          {...register("phone", { required: true })}
+        />
+        {errors.phone && <p>Phone is required</p>}
+
+        <label htmlFor="address">Address</label>
+        <input
+          id="address"
+          type="text"
+          {...register("address", { required: true })}
+        />
+        {errors.address && <p>Address is required</p>}
+
+
+        {/* Add a submit button */}
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
-
-export default App;
